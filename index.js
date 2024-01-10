f38eb81fc171fcd06f083f5e3a332b0bc22b9558
EStherMmutisya <starjacob994@gmail.com>
2024-01-10 19:26:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+//const.body.append(ul)
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent= "YOUR-NAME is the champion"
+ document.body.append(newHeader)
+
+
+
+
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+element.append(ul);
